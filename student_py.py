8dd21b4ab4b645a263ddad2d83a75ea724c9c4e1
Sara Waris <sarawaris006@gmail.com>
2026-07-13 13:19:32 +0500

    Add files via upload

---
+++ student_py.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+university= input("Enter your university:")
+
+
+print()
+print("--------------Student Details----------------")
+print("Name:", name)
+print("Age:", age)
+print("University:", university)
