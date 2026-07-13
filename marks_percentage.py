7aa6d148cfaa244651aecf3a217bd273966cd2f2
Sara Waris <sarawaris006@gmail.com>
2026-07-13 13:50:14 +0500

    Add files via upload

---
+++ marks_percentage.py
@@ -0,0 +1,9 @@
+total_marks = float(input("Enter total marks:"))
+obtained_marks = float(input("Enter obtained marks: "))
+percentage = (obtained_marks/total_marks)*100
+
+
+print("\n------percentage------")
+print("Total Marks:", total_marks)
+print("Obtained Marks:", obtained_marks)
+print("Percentage:", percentage)
