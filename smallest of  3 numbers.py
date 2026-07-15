6044c2d3f794f82b7092235d9251ae12b03ae99b
Sara Waris <sarawaris006@gmail.com>
2026-07-15 13:28:47 +0500

    Add files via upload

---
+++ smallest of  3 numbers.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+print("\n------Smallest Number------")
+
+if num1 <= num2 and num1 <= num3:
+    print("Largest number is:", num1)
+
+elif num2 <= num1 and num2 <= num3:
+    print("Largest number is:", num2)
+
+else:
+    print("Smallest number is:", num3) 
