a1e5cc448c9257e980223cc62361e84c7d479429
Sara Waris <sarawaris006@gmail.com>
2026-07-15 11:57:00 +0500

    Add files via upload

---
+++ grade_calculator.py
@@ -0,0 +1,17 @@
+marks = float(input("Enter your marks: "))
+
+
+
+print("\n------Grade------")
+if marks >= 90:
+    print("A+")
+elif marks >= 80:
+    print("A") 
+elif marks >= 70:  
+    print("B")
+elif marks >= 60:
+    print("C")
+elif marks >= 50:
+    print("D")         
+else:
+    print("Grade: Fail")    
