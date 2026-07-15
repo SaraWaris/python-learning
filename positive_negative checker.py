59aef277fd0f861da69bb886c1a669a5050d1078
Sara Waris <sarawaris006@gmail.com>
2026-07-15 13:06:40 +0500

    Add files via upload

---
+++ positive_negative checker.py
@@ -0,0 +1,12 @@
+number = int(input("Enter your number:"))
+
+
+
+print("\n-----Positive/Negative------")
+if number > 0:
+    print("Positive")
+elif number < 0:    
+    print("Negative")
+
+else:
+    print("Zero")    
