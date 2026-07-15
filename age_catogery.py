a1143483785b2cec97bdfe57630f4ee549acd9ce
Sara Waris <sarawaris006@gmail.com>
2026-07-15 12:55:57 +0500

    Add files via upload

---
+++ age_catogery.py
@@ -0,0 +1,13 @@
+age = int(input("Enter age age:"))
+
+
+
+print("\n-----Age Catogery------")
+if age < 13:
+    print("Child")
+elif age < 20:    
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior Citizen")    
