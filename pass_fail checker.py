1ef26d19865b0af7814a416729ec742a34165bba
Sara Waris <sarawaris006@gmail.com>
2026-07-15 10:42:46 +0500

    Add files via upload

---
+++ pass_fail checker.py
@@ -0,0 +1,10 @@
+marks = float(input("Enter your marks: "))
+
+
+
+print("------result------")
+if marks >= 40:
+    print("Congratulations! You are passed.")
+
+else:
+    print("Sorry! You are failled.")    
