d9800d10605fb56df52963cec6e15c620cd98828
Sara Waris <sarawaris006@gmail.com>
2026-07-16 11:52:30 +0500

    Add files via upload

---
+++ Login system.py
@@ -0,0 +1,12 @@
+username = input("Enter your name:")
+password = input("Enter your password:")
+
+print("\n-----Login System-----")
+if username == "Sara":
+      if password == "SWaris":
+           print("login successful!")
+      else:
+        print("Incorrect Pasword!")
+
+else:
+    print("username not found")    
